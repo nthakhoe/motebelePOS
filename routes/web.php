3936e69b73f7d1aedf86210e20dc0e82ec687b1a
Masilonyane Letebele <m.letebele@rsl.org.ls>
2026-07-16 13:46:59 +0200

    Update Done

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/db-test', function () {
     try {
