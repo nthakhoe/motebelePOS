6e40288763f2f21e7293c869a18ded70f4824434
Masilonyane Letebele <m.letebele@rsl.org.ls>
2026-07-16 13:45:00 +0200

    Update Done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+
+        return response()->json([
+            'status' => 'Connected',
+            'host' => config('database.connections.mysql.host'),
+            'database' => config('database.connections.mysql.database'),
+            'username' => config('database.connections.mysql.username'),
+            'password_length' => strlen(config('database.connections.mysql.password') ?? ''),
+        ]);
+    } catch (\Throwable $e) {
+        return response()->json([
+            'status' => 'Failed',
+            'host' => config('database.connections.mysql.host'),
+            'database' => config('database.connections.mysql.database'),
+            'username' => config('database.connections.mysql.username'),
+            'password_length' => strlen(config('database.connections.mysql.password') ?? ''),
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+});
 
 //Route::get('/', function () {
 //    return view('welcome');
